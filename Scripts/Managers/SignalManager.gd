0ce7a151a1b4ddc845eb5c2c8958a66145ff834d
rogeriootto <rogerio.ocupado@gmail.com>
2024-07-11 21:54:06 -0300

    fix signal

--- Scripts/Managers/SignalManager.gd
+++ Scripts/Managers/SignalManager.gd
@@ -7,18 +7,15 @@ signal interactRequest()
 
 func registerListner(signalName: String, target: Object, method: String): 
 	if not has_signal(signalName):
-		print("SignalManager: Sinal não definido - ", signalName)
 		return
 	var callable = Callable(target, method)
-	connect(signalName, callable)
-	# print("SignalManager: Sinal conectado - ", signalName)
+	if not is_connected(signalName, callable):
+		connect(signalName, callable)
 
 func unregisterListner(signalName: String, target: Object, method: String):
 	var callable = Callable(target, method)
-	print('chegou aqui')
 	if is_connected(signalName, callable):
 		disconnect(signalName, callable)
-		print("SignalManager: Sinal desconectado - ", signalName)
 
 func emitObstacleSpawnRequest(obstacleName: String, obstaclePosition: Vector3):
 	emit_signal("obstacleSpawnRequest", obstacleName, obstaclePosition)
